bfbe9a9d421796b01496dfab4d4160796f68be2b
Headgie <headgy@yandex.ru>
2020-06-09 01:25:17 +0300

    flex

---
+++ template-test/script2.js
@@ -0,0 +1,7 @@
+function toggleNav() {
+  document.getElementById("nav").classList.toggle("hidden");
+  document.getElementById("close-nav").classList.toggle("hidden");
+  document.getElementById("open-nav").classList.toggle("hidden");
+
+}
+
